6b24fc604fd5b061f83c803a7a6d4de5ddeaff53
Johannes Donath <johannesd@evil-co.com>
2014-03-12 08:47:00 -0700

    Added support for saving/loading directly into streams or files.

--- src/main/java/com/evilco/configuration/xml/ConfigurationProcessor.java
+++ src/main/java/com/evilco/configuration/xml/ConfigurationProcessor.java
@@ -1,7 +1,17 @@
 package com.evilco.configuration.xml;
 
 import com.evilco.configuration.xml.exception.ConfigurationException;
+import com.evilco.configuration.xml.exception.ConfigurationProcessorException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,6 +64,103 @@ public class ConfigurationProcessor {
 	 */
 	public static ConfigurationProcessor getInstance () {
 		return Singleton.instance;
+	}
+
+	/**
+	 * Loads an object from the specified stream.
+	 * @param inputStream
+	 * @param type
+	 * @param <T>
+	 * @return
+	 * @throws ConfigurationException
+	 */
+	public <T> T load (InputStream inputStream, Class<T> type) throws ConfigurationException {
+		try {
+			// create xml document
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance ();
+
+			// set properties
+			documentBuilderFactory.setNamespaceAware (true);
+
+			// get document
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder ();
+
+			// construct from file
+			Document document = documentBuilder.parse (inputStream);
+
+			// un-marshal
+			IUnmarshaller unmarshaller = this.createUnmarshaller (type);
+			return type.cast (unmarshaller.unmarshal (document));
+		} catch (ParserConfigurationException ex) {
+			throw new ConfigurationProcessorException (ex);
+		} catch (SAXException ex) {
+			throw new ConfigurationProcessorException (ex);
+		} catch (IOException ex) {
+			throw new ConfigurationProcessorException (ex);
+		}
+	}
+
+	/**
+	 * Loads an object from the specified stream.
+	 * @param file
+	 * @param type
+	 * @param <T>
+	 * @return
+	 * @throws ConfigurationException
+	 */
+	public <T> T load (File file, Class<T> type) throws ConfigurationException {
+		try {
+			return this.load (new FileInputStream (file), type);
+		} catch (FileNotFoundException ex) {
+			throw new ConfigurationProcessorException (ex);
+		}
+	}
+
+	/**
+	 * Saves an object into the specified stream.
+	 * @param object
+	 * @param outputStream
+	 * @return
+	 * @throws ConfigurationException
+	 */
+	public void save (Object object, OutputStream outputStream) throws ConfigurationException {
+		// create marshaller
+		IMarshaller marshaller = this.createMarshaller (object.getClass ());
+
+		// marshal object
+		Document document = marshaller.marshal (object);
+
+		// transform object
+		try {
+			Transformer transformer = TransformerFactory.newInstance ().newTransformer ();
+
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty ("{http://xml.apache.org/xslt}indent-amount", "8");
+
+			Result output = new StreamResult (outputStream);
+			Source source = new DOMSource (document);
+
+			transformer.transform (source, output);
+		} catch (TransformerConfigurationException ex) {
+			throw new ConfigurationProcessorException (ex);
+		} catch (TransformerException ex) {
+			throw new ConfigurationProcessorException (ex);
+		}
+	}
+
+	/**
+	 * Saves an object into the specified file.
+	 * @param object
+	 * @param file
+	 * @throws ConfigurationException
+	 */
+	public void save (Object object, File file) throws ConfigurationException {
+		try {
+			this.save (object, new FileOutputStream (file));
+		} catch (IOException ex) {
+			throw new ConfigurationProcessorException (ex);
+		}
 	}
 
 	/**
