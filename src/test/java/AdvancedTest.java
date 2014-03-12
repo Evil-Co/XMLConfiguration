8c529808ece55ab252c9b181567d3747ddc3441b
Johannes Donath <johannesd@evil-co.com>
2014-03-12 14:11:58 -0700

    Added tests for map extensions. Added content to lists.

--- src/test/java/AdvancedTest.java
+++ src/test/java/AdvancedTest.java
@@ -11,6 +11,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.w3c.dom.Document;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @auhtor Johannes Donath <johannesd@evil-co.com>
@@ -26,17 +28,17 @@ public class AdvancedTest {
 	@Test
 	public void extendedListTest () throws ConfigurationException {
 		// create marshaller & un-marshaller
-		IMarshaller<ConfigurationTest> marshaller = ConfigurationProcessor.getInstance ().createMarshaller (ConfigurationTest.class);
-		IUnmarshaller<ConfigurationTest> unmarshaller = ConfigurationProcessor.getInstance ().createUnmarshaller (ConfigurationTest.class);
+		IMarshaller<ConfigurationTestList> marshaller = ConfigurationProcessor.getInstance ().createMarshaller (ConfigurationTestList.class);
+		IUnmarshaller<ConfigurationTestList> unmarshaller = ConfigurationProcessor.getInstance ().createUnmarshaller (ConfigurationTestList.class);
 
 		// marshal into document
-		Document document = marshaller.marshal (new ConfigurationTest ());
+		Document document = marshaller.marshal (new ConfigurationTestList ());
 
 		// verify document
 		Assert.assertNotNull ("The resulting document is invalid", document);
 
 		// un-marshal
-		ConfigurationTest testObject = unmarshaller.unmarshal (document);
+		ConfigurationTestList testObject = unmarshaller.unmarshal (document);
 
 		// verify object
 		Assert.assertNotNull ("The resulting object is invalid", testObject);
@@ -44,17 +46,70 @@ public class AdvancedTest {
 	}
 
 	/**
+	 * Tests the marshalling and un-marshalling of maps.
+	 * @throws ConfigurationException
+	 */
+	@Test
+	public void extendedMapTest () throws ConfigurationException {
+		// create marshaller & un-marshaller
+		IMarshaller<ConfigurationTestMap> marshaller = ConfigurationProcessor.getInstance ().createMarshaller (ConfigurationTestMap.class);
+		IUnmarshaller<ConfigurationTestMap> unmarshaller = ConfigurationProcessor.getInstance ().createUnmarshaller (ConfigurationTestMap.class);
+
+		// marshal into document
+		Document document = marshaller.marshal (new ConfigurationTestMap ());
+
+		// verify document
+		Assert.assertNotNull ("The resulting document is invalid", document);
+
+		// un-marshal
+		ConfigurationTestMap testObject = unmarshaller.unmarshal (document);
+
+		// verify object
+		Assert.assertNotNull ("The resulting object is invalid", testObject);
+		Assert.assertNotNull ("The resulting map is invalid", testObject.map);
+	}
+
+	/**
 	 * A simple test configuration.
 	 */
 	@Configuration
-	public static class ConfigurationTest {
+	public static class ConfigurationTestList {
 
 		@Property ("testList")
 		public TestList list = new TestList ();
+
+		/**
+		 * Constructs a new instance of ConfigurationTestList.
+		 */
+		public ConfigurationTestList () {
+			this.list.add (42);
+		}
+	}
+
+	/**
+	 * A simple test configuration.
+	 */
+	@Configuration
+	public static class ConfigurationTestMap {
+
+		@Property ("testMap")
+		public TestMap map = new TestMap ();
+
+		/**
+		 * Constructs a new instance of ConfigurationTestMap.
+		 */
+		public ConfigurationTestMap () {
+			this.map.put ("theAnswer", 42);
+		}
 	}
 
 	/**
 	 * A simple test list.
 	 */
 	public static class TestList extends ArrayList<Integer> { }
+
+	/**
+	 * A simple test map.
+	 */
+	public static class TestMap extends HashMap<String, Integer> { }
 }
